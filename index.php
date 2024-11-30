c55480b7c9cc8a75b364168233f6b09af312a1e0
Hasan <mrzizt04@gmail.com>
2024-11-30 18:16:40 +0300

    create html table

--- index.php
+++ index.php
@@ -3,3 +3,33 @@ $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-s
 $response = file_get_contents($URL);
 $data = json_decode($response, true);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css">
+    <title>UOB Student Nationality Data</title>
+</head>
+<body>
+    <h1>UOB Student Nationality Data</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nationality</th>
+                <th>Count</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data['records'] as $record): ?>
+                <tr>
+                    <td><?php echo $record['fields']['nationality']; ?></td>
+                    <td><?php echo $record['fields']['count']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
